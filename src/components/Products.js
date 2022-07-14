070d6c9b7cfa67729a09aa0c03db9b0bdf1ec52c
Gaurav-2809 <smartguptagaurav01@gmail.com>
2022-07-14 11:51:17 +0530

    edit

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,18 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Product from '../components/Product'
 
 const Products = () => {
-    
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        const options = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': 'e4e9ac2881mshd1db3270dfd6fc5p18a338jsn652a76b8dddd',
+                'X-RapidAPI-Host': 'pizzaallapala.p.rapidapi.com'
+            }
+        };
+        
+        fetch('https://pizzaallapala.p.rapidapi.com/productos', options)
+            .then(response => response.json())
+            .then(products => {
+                setProducts(products);
+            });
+        
+    },[]);
     return (
         <>
             <div className='container mx-auto pb-24'>
                 <h1 className='font-bold text-lg my-8'>Products</h1>
                 <div className='grid grid-cols-5 gap-24 my-8'>
-                    <Product/>
+                    {
+                        products.map((product) => {<Product key={product.id} />})
+                    }
                 </div>
             </div>
         </>
     )
 }
 
-export default Products
+export default Products;
